bb1dd6f5ed8091a0884690aa651df11f51837779
Tien Nguyen Nhat <anonymous209184@gmail.com>
2020-08-28 14:01:52 +0700

    done notificationVC

--- ECB-Wallet/Controllers/NotificationViewController.swift
+++ ECB-Wallet/Controllers/NotificationViewController.swift
@@ -8,9 +8,11 @@
 
 import UIKit
 
-class NotificationViewController: UIViewController {
+class NotificationViewController: UIViewController, UIGestureRecognizerDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+    var gestureRow : [NSValue: Int]  = [:]
+    var gestureSession:[NSValue: Int]  = [:]
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -30,20 +32,74 @@ class NotificationViewController: UIViewController {
 }
 extension NotificationViewController:UITableViewDataSource, UITableViewDelegate{
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return 3
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 5
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "NotificationTableViewCell", for: indexPath) as! NotificationTableViewCell
-        
+        //
+        let drag = UIPanGestureRecognizer(target: self, action: #selector(cellDragged(_:)))
+        drag.delegate = self
+        cell.addGestureRecognizer(drag)
+        let value = NSValue.init(nonretainedObject: drag)
+        gestureRow[value] = indexPath.row
+        gestureSession[value] = indexPath.section
         return cell
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 110
     }
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: self.tableView.frame.size.width, height: 40))
+        let imageView = UIImageView(frame: CGRect(x: 15, y: 10, width: 20, height: 20))
+        imageView.image = UIImage(named: "redCircel")
+        
+        let titleLabel = UILabel(frame: CGRect(x: 45, y: 10, width: 150, height: 21))
+        titleLabel.font = UIFont(name: "SF Pro Display", size: 18)
+        titleLabel.textColor = UIColor(red: 16/255, green: 0/255, blue: 54/255, alpha: 1)
+        titleLabel.text = "06 Aug 2020"
+        headerView.addSubview(imageView)
+        headerView.addSubview(titleLabel)
+        return headerView
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
+    }
+    //MARK: UIGestureRecognizerDelegate
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        if let panGestureRecognizer = gestureRecognizer as? UIPanGestureRecognizer {
+            //let translation = panGestureRecognizer.translation(in: superview!)
+            let translation = panGestureRecognizer.translation(in: tableView)
+            if abs(translation.x) > abs(translation.y) {
+                return true
+            }
+            return false
+        }
+        return false
+    }
     //MARK: helper method
-    
+    @objc func cellDragged(_ gesture: UIPanGestureRecognizer) {
+        let key = NSValue.init(nonretainedObject: gesture)
+        guard let row = gestureRow[key] else {return}
+        guard let session = gestureSession[key] else {return}
+        let indexPath = IndexPath(row: row, section: session)
+        let cell = tableView.cellForRow(at: indexPath) as! NotificationTableViewCell
+        let translation = gesture.translation(in: cell)
+        if translation.x < 0 {
+            UIView.animate(withDuration: 0.5) {
+                cell.trailingCellConstant.constant = 50
+                cell.leadingCellConstant.constant = 0
+            }
+            cell.trailingCellConstant.constant = 50
+            cell.leadingCellConstant.constant = 0
+        }else{
+            UIView.animate(withDuration: 0.5) {
+                cell.trailingCellConstant.constant = 0
+                cell.leadingCellConstant.constant = 50
+            }
+        }
+    }
 }
--- ECB-Wallet/Views/NotificationTableViewCell.swift
+++ ECB-Wallet/Views/NotificationTableViewCell.swift
@@ -10,10 +10,20 @@ import UIKit
 
 class NotificationTableViewCell: UITableViewCell {
 
+    
+    
+    @IBOutlet weak var containerView: BoundFourCornersView!
     @IBOutlet weak var trailingCellConstant: NSLayoutConstraint!
+    @IBOutlet weak var leadingCellConstant: NSLayoutConstraint!
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var notificationTitleLabel: UILabel!
+    @IBOutlet weak var notificationContentLabel: UILabel!
+    @IBOutlet weak var unreadImage: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        containerView.layer.borderWidth = 1
+        containerView.layer.borderColor = UIColor(red: 226/255, green: 226/255, blue: 226/255, alpha: 1).cgColor
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -21,5 +31,10 @@ class NotificationTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    @IBAction func deleteButtonWasPressed(_ sender: Any) {
+    }
     
+    @IBAction func hideButtonWasPressed(_ sender: Any) {
+    }
+    //Write a function to configure Cell in here
 }
