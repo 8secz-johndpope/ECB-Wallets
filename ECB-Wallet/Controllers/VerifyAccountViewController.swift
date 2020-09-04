8d360a1d6d0f70e5e0717ad0a8e3c6705572e5a8
Tien Nguyen Nhat <anonymous209184@gmail.com>
2020-09-04 23:29:50 +0700

    fix verifyAcount and VerifyForgetPassword

--- ECB-Wallet/Controllers/VerifyAccountViewController.swift
+++ ECB-Wallet/Controllers/VerifyAccountViewController.swift
@@ -23,6 +23,7 @@ class VerifyAccountViewController: UIViewController {
         // Do any additional setup after loading the view.
         // set textField Delegate
         OTPTextField1.becomeFirstResponder()
+        
         OTPTextField1.delegate = self
         OTPTextField2.delegate = self
         OTPTextField3.delegate = self
@@ -34,7 +35,10 @@ class VerifyAccountViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
     @IBAction func confirmButtonWasPressed(_ sender: Any) {
-        self.performSegue(withIdentifier: "goToFinishSignUpVC", sender: nil)
+        let OTP = "\(OTPTextField1.text!)\(OTPTextField2.text!)\(OTPTextField3.text!)\(OTPTextfield4.text!)\(OTPTextField5.text!)\(OTPTextField6.text!)"
+        if OTP == "123456"{
+            self.performSegue(withIdentifier: "goToFinishSignUpVC", sender: nil)
+        }
     }
     @IBAction func resendOPTButtonWasPressed(_ sender: Any) {
         print("Resend OTP")
@@ -43,19 +47,80 @@ class VerifyAccountViewController: UIViewController {
 }
 //
 extension VerifyAccountViewController: UITextFieldDelegate{
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        if textField == OTPTextField1{
-            OTPTextField2.becomeFirstResponder()
-        }else if textField == OTPTextField2{
-            OTPTextField3.becomeFirstResponder()
-        }else if textField == OTPTextField3{
-            OTPTextfield4.becomeFirstResponder()
-        }else if textField == OTPTextfield4{
-            OTPTextField5.becomeFirstResponder()
-        }else if textField == OTPTextField5{
-            OTPTextField6.becomeFirstResponder()
-        }else{
-            OTPTextField6.resignFirstResponder()
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if textField.text!.count < 1 && string.count > 0{
+            if textField == OTPTextField1{
+                OTPTextField2.text = ""
+                OTPTextField2.becomeFirstResponder()
+            }
+            if textField == OTPTextField2{
+                OTPTextField3.text = ""
+                OTPTextField3.becomeFirstResponder()
+            }
+            if textField == OTPTextField3{
+                OTPTextfield4.text = ""
+                OTPTextfield4.becomeFirstResponder()
+            }
+            if textField == OTPTextfield4{
+                OTPTextField5.text = ""
+                OTPTextField5.becomeFirstResponder()
+            }
+            if textField == OTPTextField5{
+                OTPTextField6.text = ""
+                OTPTextField6.becomeFirstResponder()
+            }
+            if textField == OTPTextField6{
+                OTPTextField6.resignFirstResponder()
+            }
+            textField.text = string
+            return false
+        }else if textField.text!.count >= 1 && string.count == 0 {
+            if textField == OTPTextField2{
+                OTPTextField1.becomeFirstResponder()
+            }
+            if textField == OTPTextField3{
+                OTPTextField2.becomeFirstResponder()
+            }
+            if textField == OTPTextfield4{
+                OTPTextField3.becomeFirstResponder()
+            }
+            if textField == OTPTextField5{
+                OTPTextfield4.becomeFirstResponder()
+            }
+            if textField == OTPTextField6{
+                OTPTextField5.becomeFirstResponder()
+            }
+            if textField == OTPTextField1{
+                OTPTextField1.resignFirstResponder()
+            }
+            textField.text = "-"
+            return false
+        }else if textField.text!.count >= 1{
+            if textField == OTPTextField1{
+                OTPTextField2.text = ""
+                OTPTextField2.becomeFirstResponder()
+            }
+            if textField == OTPTextField2{
+                OTPTextField3.text = ""
+                OTPTextField3.becomeFirstResponder()
+            }
+            if textField == OTPTextField3{
+                OTPTextfield4.text = ""
+                OTPTextfield4.becomeFirstResponder()
+            }
+            if textField == OTPTextfield4{
+                OTPTextField5.text = ""
+                OTPTextField5.becomeFirstResponder()
+            }
+            if textField == OTPTextField5{
+                OTPTextField6.text = ""
+                OTPTextField6.becomeFirstResponder()
+            }
+            if textField == OTPTextField6{
+                OTPTextField6.resignFirstResponder()
+            }
+            textField.text = string
+            return false
         }
         return true
     }
--- ECB-Wallet/Controllers/VerifyForgotPasswordViewController.swift
+++ ECB-Wallet/Controllers/VerifyForgotPasswordViewController.swift
@@ -22,6 +22,15 @@ class VerifyForgotPasswordViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        // set textField Delegate
+        OTPTextfield1.becomeFirstResponder()
+               
+        OTPTextfield1.delegate = self
+        OTPTextField2.delegate = self
+        OTPTextField3.delegate = self
+        OTPTextField4.delegate = self
+        OTPTextField5.delegate = self
+        OTPTextField6.delegate = self
     }
     
 
@@ -29,9 +38,92 @@ class VerifyForgotPasswordViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
     @IBAction func confirmButtonwasPressed(_ sender: Any) {
-        self.performSegue(withIdentifier: "goToCreateNewPasswordVC", sender: nil)
+        let OTP = "\(OTPTextfield1.text!)\(OTPTextField2.text!)\(OTPTextField3.text!)\(OTPTextField4.text!)\(OTPTextField5.text!)\(OTPTextField6.text!)"
+        if OTP == "123456"{
+            self.performSegue(withIdentifier: "goToCreateNewPasswordVC", sender: nil)
+        }
     }
     @IBAction func resendOTPButtonWasPressed(_ sender: Any) {
     }
     
 }
+extension VerifyForgotPasswordViewController: UITextFieldDelegate{
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if textField.text!.count < 1 && string.count > 0{
+            if textField == OTPTextfield1{
+                OTPTextField2.text = ""
+                OTPTextField2.becomeFirstResponder()
+            }
+            if textField == OTPTextField2{
+                OTPTextField3.text = ""
+                OTPTextField3.becomeFirstResponder()
+            }
+            if textField == OTPTextField3{
+                OTPTextField4.text = ""
+                OTPTextField4.becomeFirstResponder()
+            }
+            if textField == OTPTextField4{
+                OTPTextField5.text = ""
+                OTPTextField5.becomeFirstResponder()
+            }
+            if textField == OTPTextField5{
+                OTPTextField6.text = ""
+                OTPTextField6.becomeFirstResponder()
+            }
+            if textField == OTPTextField6{
+                OTPTextField6.resignFirstResponder()
+            }
+            textField.text = string
+            return false
+        }else if textField.text!.count >= 1 && string.count == 0 {
+            if textField == OTPTextField2{
+                OTPTextfield1.becomeFirstResponder()
+            }
+            if textField == OTPTextField3{
+                OTPTextField2.becomeFirstResponder()
+            }
+            if textField == OTPTextField4{
+                OTPTextField3.becomeFirstResponder()
+            }
+            if textField == OTPTextField5{
+                OTPTextField4.becomeFirstResponder()
+            }
+            if textField == OTPTextField6{
+                OTPTextField5.becomeFirstResponder()
+            }
+            if textField == OTPTextfield1{
+                OTPTextfield1.resignFirstResponder()
+            }
+            textField.text = "-"
+            return false
+        }else if textField.text!.count >= 1{
+            if textField == OTPTextfield1{
+                OTPTextField2.text = ""
+                OTPTextField2.becomeFirstResponder()
+            }
+            if textField == OTPTextField2{
+                OTPTextField3.text = ""
+                OTPTextField3.becomeFirstResponder()
+            }
+            if textField == OTPTextField3{
+                OTPTextField4.text = ""
+                OTPTextField4.becomeFirstResponder()
+            }
+            if textField == OTPTextField4{
+                OTPTextField5.text = ""
+                OTPTextField5.becomeFirstResponder()
+            }
+            if textField == OTPTextField5{
+                OTPTextField6.text = ""
+                OTPTextField6.becomeFirstResponder()
+            }
+            if textField == OTPTextField6{
+                OTPTextField6.resignFirstResponder()
+            }
+            textField.text = string
+            return false
+        }
+        return true
+    }
+}
+
